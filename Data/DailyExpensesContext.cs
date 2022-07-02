e97d74f7683d2c00280e76b21dc1d7bb084ffd0c
Carlos Diaz <carlos.cedp@gmail.com>
2022-07-02 10:53:34 -0400

    first setup

---
+++ Data/DailyExpensesContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWallet.Model;
+
+namespace MyWallet.Data
+{
+    public class DailyExpensesContext: DbContext
+    {
+        public DailyExpensesContext(DbContextOptions<DailyExpensesContext> contextOptions): base(contextOptions) { }
+        public DailyExpensesContext() { }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
---
+++ Model/Expense.cs
@@ -0,0 +1,9 @@
+﻿namespace MyWallet.Model
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
